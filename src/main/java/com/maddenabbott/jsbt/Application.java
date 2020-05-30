208f34d3a8008df7753cf3d3bb295a2d621adc22
Rupert Madden-Abbott <rupert@madden-abbott.com>
2020-05-30 20:34:19 +0100

    Adjust to replicate issue.

---
+++ src/main/java/com/maddenabbott/jsbt/Application.java
@@ -0,0 +1,24 @@
+package com.maddenabbott.jsbt;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    private final Environment environment;
+
+    public Application(Environment environment) {
+        this.environment = environment;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(environment.getRequiredProperty("test"));
+    }
+}
